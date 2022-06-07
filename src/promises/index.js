ecaf199decfead5029cb4e866c65230c5d4fdb74
Jonathan Ortiz <dual.jonathan.ortiz@gmail.com>
2022-06-07 01:05:11 -0500

    Cambios finales

---
+++ src/promises/index.js
@@ -0,0 +1,48 @@
+const somethingWillHapped = () => {
+    return new Promise( (resolve, reject) => {
+        if (true) {
+            resolve('Hey!');
+        } else {
+            reject('Whoops!');
+        }
+    });
+};
+
+somethingWillHapped()
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+
+const somethingWillHapped2 = () =>
+{
+    return new Promise((resolve, reject) =>
+    {
+        if(true)
+        {
+            setTimeout(() =>
+            {
+                resolve('True');
+            }, 2000)
+        }
+        else
+        {
+            const error = new Error('Whoooop!');
+            reject(error);
+        }
+    });
+}
+
+somethingWillHapped2()
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+Promise.all([somethingWillHapped(), somethingWillHapped2()])
+    .then(response => 
+        {
+            console.log('Array of results', response);
+        })
+        .catch(err =>
+        {
+            console.error(err);
+        })
+    
